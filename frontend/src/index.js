7734c6f8e48263996545aebb4c240d4235872fb5
Connecting Blockchains <ceo@ideamart.ai>
2024-08-26 14:52:54 -0700

    Create index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,24 @@
+// App.js
+import React from 'react';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Dashboard from './components/Dashboard';
+import SwapForm from './components/SwapForm';
+import TransactionHistory from './components/TransactionHistory';
+import './App.scss';
+
+const App = () => {
+    return (
+        <div className="app">
+            <Navbar />
+            <main>
+                <Dashboard />
+                <SwapForm />
+                <TransactionHistory />
+            </main>
+            <Footer />
+        </div>
+    );
+};
+
+export default App;
